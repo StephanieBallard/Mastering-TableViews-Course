dd85dc63ba8f06896030d6318e1a96ca1b96aedf
Stephanie Ballard <user.name>
2020-03-04 18:49:43 -0800

    section 2 done

--- MasteringTableViewsCourseBeginnerSection/populatingMultipleSectionsWithArray/populatingMultipleSectionsWithArray/ViewController.swift
+++ MasteringTableViewsCourseBeginnerSection/populatingMultipleSectionsWithArray/populatingMultipleSectionsWithArray/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let fruit = [["Orange", "Apple", "Pear"], ["I", "Love", "Fruit!"], ["Hello", "World!"]]
+    
+    let headerText = ["Fruits", "What I love", "Textbook Text"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fruit.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruit[section].count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = fruit[indexPath.section][indexPath.row] //section zero, row zero, this gets called for each indexpath.row as the table view moves
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headerText[section]
+    }
 }
 
