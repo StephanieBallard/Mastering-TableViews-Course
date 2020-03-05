ed90487f196344f2cc39e4ffd2f1c110d6f7a43f
Stephanie Ballard <user.name>
2020-03-04 21:39:30 -0800

    saving

--- MasteringTableViewsCourseBeginnerSection/populatingMultipleSectionsWithArray/populatingMultipleSectionsWithArray/MultipleSections.swift
+++ MasteringTableViewsCourseBeginnerSection/populatingMultipleSectionsWithArray/populatingMultipleSectionsWithArray/MultipleSections.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class MultipleSections: UIViewController, UITableViewDataSource {
-
-    let fruit = [["Orange", "Apple", "Pear"], ["I", "Love", "Fruit!"], ["Hello", "World!"]]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var fruit = [["Orange", "Apple", "Pear"], ["I", "Love", "Fruit!"], ["Hello", "World!"]]
     
     let headerText = ["Fruits", "What I love", "Textbook Text"]
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,5 +40,21 @@ class MultipleSections: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return headerText[section]
     }
+    
+    @IBAction func sortArray(_ sender: UIButton) {
+        fruit[0].sort()
+        // call reload data for the sort button to work. once the tableview has been populated you need to reload the data. the array will sort but wont show the changes in the table unless reload data is called.
+        //tableView.reloadData()
+        
+        // if you only want to reload the one cell
+        //let indexPath = IndexPath(row: 0, section: 0)
+        // UITableView.RowAnimation can be removed for cleaner code.
+        // this will make apple appear twice since we are only reloading the one cell, however this is how you reload one cell.
+        //tableView.reloadRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        
+        // this will only reload the first section
+        tableView.reloadSections([0], with: .fade)
+    }
+    
 }
 
