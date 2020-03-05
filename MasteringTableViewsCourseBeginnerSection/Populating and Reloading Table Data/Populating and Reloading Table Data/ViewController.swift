a6ca5520b265053f8a2872bcefb22c8aa37713c8
Stephanie Ballard <user.name>
2020-03-04 18:51:18 -0800

    section 3 set up

---
+++ MasteringTableViewsCourseBeginnerSection/Populating and Reloading Table Data/Populating and Reloading Table Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Populating and Reloading Table Data
+//
+//  Created by Stephanie Ballard on 3/4/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
