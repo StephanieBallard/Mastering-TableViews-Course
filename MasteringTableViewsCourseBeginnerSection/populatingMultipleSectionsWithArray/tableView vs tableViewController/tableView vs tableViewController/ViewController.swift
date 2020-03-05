692c0010fd2c2cac19ef97211cbc4c4c2d6271bd
Stephanie Ballard <user.name>
2020-03-04 22:54:12 -0800

    finished section

--- MasteringTableViewsCourseBeginnerSection/populatingMultipleSectionsWithArray/tableView vs tableViewController/tableView vs tableViewController/ViewController.swift
+++ MasteringTableViewsCourseBeginnerSection/populatingMultipleSectionsWithArray/tableView vs tableViewController/tableView vs tableViewController/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
