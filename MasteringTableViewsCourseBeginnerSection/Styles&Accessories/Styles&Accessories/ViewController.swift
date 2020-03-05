ae8076ea96b52914f525c668523ab8c12bcdb675
Stephanie Ballard <user.name>
2020-03-05 02:45:41 -0800

    section finished

--- MasteringTableViewsCourseBeginnerSection/Styles&Accessories/Styles&Accessories/ViewController.swift
+++ MasteringTableViewsCourseBeginnerSection/Styles&Accessories/Styles&Accessories/ViewController.swift
@@ -18,3 +18,37 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Accessorize me!"
+        cell.detailTextLabel?.text = "My Detail"
+        
+        switch indexPath.row {
+        case 0:
+            cell.accessoryType = .checkmark
+        case 1:
+            cell.accessoryType = .detailButton
+        case 2:
+            cell.accessoryType = .detailDisclosureButton
+        case 3:
+            cell.accessoryType = .disclosureIndicator
+        case 4:
+            cell.accessoryType = .none
+        default:
+            break
+        }
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        print(indexPath.row, "was selected")
+    }
+}
