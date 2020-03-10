47e8325b00593bf89aa26a68d08cb2ba2ceb2c3f
Stephanie Ballard <user.name>
2020-03-09 21:31:33 -0700

    indexes

--- MasteringTableViewsIntermediateSection/headers and footers/headers and footers/ViewController.swift
+++ MasteringTableViewsIntermediateSection/headers and footers/headers and footers/ViewController.swift
@@ -11,18 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     let headerText = ["Header 1", "Header2", "Header 3", "Header 4"]
+    
+    let aToZ = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -30,29 +29,33 @@ extension ViewController: UITableViewDataSource {
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return headerText.count
+        return aToZ.count
     }
 }
 
 extension ViewController: UITableViewDelegate {
     
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        return headerText[section]
-//    }
-//
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return aToZ
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return aToZ[section]
+    }
+
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
           return 28
       }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = UIView()
-        let label = UILabel(frame: (CGRect(x: 10, y: 10, width: 100, height: 20)))
-        label.text = headerText[section]
-        header.addSubview(label)
-        header.backgroundColor = .white
-        return header
-    }
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let header = UIView()
+//        let label = UILabel(frame: (CGRect(x: 10, y: 10, width: 100, height: 20)))
+//        label.text = headerText[section]
+//        header.addSubview(label)
+//        header.backgroundColor = .white
+//        return header
+//    }
 //
 //    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
 //        return 28
