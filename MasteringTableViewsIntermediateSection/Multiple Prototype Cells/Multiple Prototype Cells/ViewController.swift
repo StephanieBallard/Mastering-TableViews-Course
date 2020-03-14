2279a0c8b263668a2136c7bd14a442aa84773a0c
Stephanie Ballard <user.name>
2020-03-14 14:56:39 -0700

    set up project

---
+++ MasteringTableViewsIntermediateSection/Multiple Prototype Cells/Multiple Prototype Cells/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Multiple Prototype Cells
+//
+//  Created by Stephanie Ballard on 3/14/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+let knobs = [#imageLiteral(resourceName: "knob1"), #imageLiteral(resourceName: "knob2"), #imageLiteral(resourceName: "knob3"), #imageLiteral(resourceName: "knob4"), #imageLiteral(resourceName: "knob5"), #imageLiteral(resourceName: "knob6")]
+let knockers = [#imageLiteral(resourceName: "knocker1"), #imageLiteral(resourceName: "knocker2"), #imageLiteral(resourceName: "knocker3"), #imageLiteral(resourceName: "knocker4"), #imageLiteral(resourceName: "knocker5")]
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
+
